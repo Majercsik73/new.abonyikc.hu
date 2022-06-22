09676f757ff3d414cda8320eb328a31508ac8d99
Majercsik73 <riasztotnekem@gmail.com>
2022-06-22 21:40:22 +0200

    Fájl törlések, csak a váz marad

--- Insert_u12.sql
+++
@@ -1,7 +0,0 @@
-
-USE `abonyikc`;
-
-INSERT INTO `lu12` (`datum`, `csapatok`, `eredmeny`) VALUES
-('2022-05-29','Gyáli BKSE - Abonyi KC.','8 - 16'),
-('2022-05-29','Abonyi KC. - Malév SC. 2.','17 - 15')
-;
--- Insert_u14.sql
+++
@@ -1,7 +0,0 @@
-
-USE `abonyikc`;
-
-INSERT INTO `lu14` (`datum`, `csapatok`, `eredmeny`) VALUES
-('2022-05-29','Tiszakécse VSE. - Abonyi KC.','0 - 10'),
-('2022-05-29','Abonyi KC. - Békéscsabai ENKSE.','17 - 30')
-;
